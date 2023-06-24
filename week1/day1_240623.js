4834fa5190bbfc80fa27972c1166bca01aca1fc7
HimAYouN <himayoun7931@gmail.com>
2023-06-24 13:06:47 +0530

    Day1commit24-06-23/13:06

---
+++ week1/day1_240623.js
@@ -0,0 +1,35 @@
+//Introduction
+
+
+//Variables
+    // Declaration : 'var', 'let', 'const'
+
+    let x = 12;
+    var a = 33;
+    const b = 23;
+    
+    // Data Types
+        // Strings 'Hello', "Hello"
+        //Numbers 8, 7.21
+        //Boolean 'true', 'false'
+        //Undefined  : Represents a variable that has been declared but not assigned a value
+        // Null: Represents intentional absence of valu
+        // Object: Key-value pairs or collection of properties
+        // Arrays: Ordered lists of values
+    var msg = "Hello There";
+    let count = 10;
+    const isTrue = true;
+    
+    //Operators
+        //Arithmetic operators + (addition), - (subtraction), * (multiplication), / (division), % (modulus).
+        //Comparison Operators: == (equality), != (inequality), > (greater than), < (less than), >= (greater than or equal to), <= (less than or equal to).
+        //Logical Operators: && (logical AND), || (logical OR), ! (logical NOT).
+        //Assignment Operators: = (assign value), += (add and assign), -=, *=, /=, %= (similarly for other arithmetic operators).
+        //Increment and Decrement Operators: ++ (increment by 1), -- (decrement by 1).
+    var p = 5;
+    var y = 6;
+    var z = p+y;
+    
+    
+    
+    
