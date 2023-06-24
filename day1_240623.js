34efdff9601448a3eadfa4c7a0704e9d614f1c81
HimAYouN <himayoun7931@gmail.com>
2023-06-24 12:58:14 +0530

    first push

---
+++ day1_240623.js
@@ -0,0 +1,21 @@
+//Introduction
+
+
+//Variables
+    // Declaration : 'var', 'let', 'const'
+
+let x = 12;
+var a = 33;
+const b = 23;
+
+// Data Types
+    // Strings 'Hello', "Hello"
+    //Numbers 8, 7.21
+    //Boolean 'true', 'false'
+    //Undefined  : Represents a variable that has been declared but not assigned a value
+    // Null: Represents intentional absence of valu
+    // Object: Key-value pairs or collection of properties
+    // Arrays: Ordered lists of values
+var msg = "Hello There";
+let count = 10;
+const isTrue = true;
