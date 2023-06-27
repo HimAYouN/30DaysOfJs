24e2bdbf6425c29af8e8beeaa3499ae73a6730c7
HimAYouN <himayoun7931@gmail.com>
2023-06-27 21:21:51 +0530

    27-06-23/21:21

--- week1/tests/test02.js
+++ week1/tests/test02.js
@@ -1,5 +1,18 @@
+
 var myButton = document.getElementById("myButton");
-var userN = document.getElementById("usernameElement");
+var confermation = document.getElementById("confermation");
 myButton.addEventListener("click", function(){
-    
+    var userN = getValue("uName");
+    var pswrd = getValue("password");
+
+    // window.alert(userN);
+    // window.alert(pswrd);
+    confermation.innerHTML = "Your credentials has been submitted <button>Click Here</button> to see your Username.";
+
 });
+
+function getValue(id) {
+    var text = document.getElementById(id).value;
+    // alert(text);
+    return text;
+}
