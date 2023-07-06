326826ea1dc00e28e4a17db10419a7e268beeb50
HimAYouN <himayoun7931@gmail.com>
2023-07-06 16:20:09 +0530

    06-07-23/16:20

--- week2/day9_060723.js
+++ week2/day9_060723.js
@@ -13,4 +13,18 @@
         }, 2000);
     });
 
-    //In this example, we create a Promise that simulates an asynchronous operation using 'setTimeout'. After a delay of 2 seconds, the Promise is resolved with the message "Good day!" using the 'resolve()' function.
+    //In this example, we create a Promise that simulates an asynchronous operation using 'setTimeout'. After a delay of 2 seconds, the Promise is resolved with the message "Good day!" using the 'resolve()' function.    //In this example, we create a Promise that simulates an asynchronous operation using 'setTimeout'. After a delay of 2 seconds, the Promise is resolved with the message "Good day!" using the 'resolve()' function.
+//Handling Asynchronous Opeartions with Promises:
+    //Promises provide methods to handle the fulfillment or rejection of the asynchronous task. 
+    //The most common methods are:
+        //'then()' : Attaches a callback function to handle the fulfillment of the Promise.
+        //'catch()' : Attaches a callback function to handle the rejection of the promise.
+
+    //Example
+    myPromise.then(function(result){
+        console.log(result); // Output: Good day
+    }).catch(function(error){
+        console.log(error);
+    });
+
+    //In the example, we use 'then()' method to handle the fulfillment of the promise. The callback function 
