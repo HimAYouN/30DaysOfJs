7966ada85cbd52c5557292e7641f798c917a2d52
HimAYouN <himayoun7931@gmail.com>
2023-07-18 09:53:14 +0530

    Update day12_170723.js

--- week2/day12_170723.js
+++ week2/day12_170723.js
@@ -32,3 +32,11 @@ higherOrderFunction(()=>{
 
     var closure = outer();
     closure();
+
+
+    //In the example,  the inner function forms a closure with its outer scope, allowing it to access the outerVariable even after the outer function has finished executing
+    //The outer function returns the inner function, which is assigned to the closure variable. When closure is called, it logs the value of outerVariable to the console.
+
+    // Scope:
+        // Scope refers to the accessibility and visibility of variables, functions, and objects within a particular part of your code during runtime. It determines the lifetime and availability of variables and helps prevent naming conflicts.
+        //In JavaScript, variables declared with the var keyword have function scope, while variables declared with the let and const keywords have block scope.
