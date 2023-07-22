f9d2ede90f4ff4f0875d4b90b23d27a508b70397
HimAYouN <himayoun7931@gmail.com>
2023-07-22 11:04:58 +0530

    made it but code sy zyda toh bugs h

--- week2/moreOnSameTopics/countDownTimer/script.js
+++ week2/moreOnSameTopics/countDownTimer/script.js
@@ -3,12 +3,73 @@ const minute = document.getElementById("minuteH3");
 const second = document.getElementById("secondH3");
 const button = document.getElementById("StartTimer");
 
+///IDk what this does
+const minutes = 1;
+//changing the minutes into seconds
+let time = minutes * 60;
 
-hour.innerHTML= "55";
+button.addEventListener("click", startTimer());
+function startTimer(){
+    console.log("Button clicked");
+    setInterval(()=>{
 
-button.addEventListener('click', ()=>{
-    hour.innerHTML = "00";
-    minute.innerHTML = "30";
-    second.innerHTML = "00";
-})
+        const minutes = Math.floor(time/60);//getting minutes out from total seconds which were previously created by conveting minutes to seconds
+        let seconds = time % 60; // getting remaining seconds from total seconds by using mod;
+        
+        // if(minutes<10){
+        //     minutes = '0' + minutes;
+        // }
+        minute.innerHTML = minutes; //displaying minutes 
+        if(seconds<10){
+            seconds = '0' + seconds;
+        }
+        second.innerHTML = seconds; // displaying seconds
+        console.log(seconds);
+        time--;
+        // if(time == 0){
+        //     playSound();
+        // }
+        if(time == 0){
+            return 0;
+        }
+    
+    }, 1000);
+}
+
+// playSound(){
+//     console.log("Times UP");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Current time
+// var currTime = new Date.getTime();
+
+
+// hour.innerHTML= "55";
+
+// button.addEventListener('click', ()=>{
+//     hour.innerHTML = "00";
+//     minute.innerHTML = "30";
+//     second.innerHTML = "00";
+// })
 
