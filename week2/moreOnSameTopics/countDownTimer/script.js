e7b22ff4cd0b7a756dd6b83c364009b176db56e3
HimAYouN <himayoun7931@gmail.com>
2023-07-23 17:04:43 +0530

    Noting

--- week2/moreOnSameTopics/countDownTimer/script.js
+++ week2/moreOnSameTopics/countDownTimer/script.js
@@ -12,8 +12,9 @@ const buttons = document.querySelectorAll('.button');
 buttons.forEach(button=>{
     button.addEventListener('click', function(event){
         const value = event.target.value;
-        console.log(value);
+        // console.log(value);
         startTimer(value);
+        
 
 
     });
@@ -57,46 +58,6 @@ function startTimer(value){
 
 
 
-// button30.addEventListener('click', ()=>{
-//     // console.log("Hello");
-    
-//     ///Setting the time in minutes
-//     const minutes = 30;
-//     //changing the minutes into seconds
-//     let time = minutes * 60;
-//     const interval = setInterval(()=>{
-
-
-//         //clears the interval and calls the playsound function 
-//         if(time == 0){
-//             playSound();
-//             clearInterval(interval);
-//         }
-
-
-
-//         let minutes = Math.floor(time/60);//getting minutes out from total seconds which were previously created by conveting minutes to seconds
-//         let seconds = time % 60; // getting remaining seconds from total seconds by using mod;
-        
-//         if(minutes<10){
-//             minutes = '0' + minutes;
-//         }
-
-
-//         minute.innerHTML = minutes; //displaying minutes 
-//         if(seconds<10){
-//             seconds = '0' + seconds;
-//         }
-//         second.innerHTML = seconds; // displaying seconds
-//         // console.log(seconds);
-//         time--;
-        
-//     }, 1000);
-// });
-
-
-
-
 
 ///plays the timer ended  sound 
 function playSound(){
@@ -110,31 +71,5 @@ function playSound(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Current time
-// var currTime = new Date.getTime();
-
-
-// hour.innerHTML= "55";
-
-// button.addEventListener('click', ()=>{
-//     hour.innerHTML = "00";
-//     minute.innerHTML = "30";
-//     second.innerHTML = "00";
-// })
+// Hiding Buttons
 
