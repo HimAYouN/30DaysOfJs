17dd6abefbb718ea1f8eb081ff4db7fbd9ca0a14
HimAYouN <himayoun7931@gmail.com>
2023-07-23 10:34:07 +0530

    Fixed Automatic Button Click

--- week2/moreOnSameTopics/countDownTimer/script.js
+++ week2/moreOnSameTopics/countDownTimer/script.js
@@ -3,12 +3,48 @@ const minute = document.getElementById("minuteH3");
 const second = document.getElementById("secondH3");
 const button = document.getElementById("StartTimer");
 
-///IDk what this does
-const minutes = 0.1;
-//changing the minutes into seconds
-let time = minutes * 60;
 
-button.addEventListener("click", startTimer());
+button.addEventListener('click', ()=>{
+    console.log("Hello");
+    
+    ///Setting the time in minutes
+    const minutes = 0.1;
+    //changing the minutes into seconds
+    let time = minutes * 60;
+    const interval = setInterval(()=>{
+
+        if(time == 0){
+            clearInterval(interval);
+        }
+        const minutes = Math.floor(time/60);//getting minutes out from total seconds which were previously created by conveting minutes to seconds
+        let seconds = time % 60; // getting remaining seconds from total seconds by using mod;
+        
+        // if(minutes<10){
+        //     minutes = '0' + minutes;
+        // }
+        minute.innerHTML = minutes; //displaying minutes 
+        if(seconds<10){
+            seconds = '0' + seconds;
+        }
+        second.innerHTML = seconds; // displaying seconds
+        console.log(seconds);
+        time--;
+        // if(time == 0){
+        //     playSound();
+        // }
+        
+    }, 1000);
+});
+
+
+function debuggerButton(){
+    console.log("Button clicked ");
+}
+
+
+
+
+
 function startTimer(){
     console.log("Button clicked");
     const interval = setInterval(()=>{
