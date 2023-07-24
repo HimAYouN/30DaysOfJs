aeb626368e4098933272485db3c5c81796806987
HimAYouN <himayoun7931@gmail.com>
2023-07-24 21:32:48 +0530

    Update main.js

--- week3/Rock,Paper,Scissors/main.js
+++ week3/Rock,Paper,Scissors/main.js
@@ -3,27 +3,33 @@ var playerScore = 0;
 var computerScore = 0;
 
 function play(myPlay){
-    
+    scoreCheck(computerScore, playerScore);
     var comPlay = Math.floor(Math.random()*3)+1;
 
     if(myPlay==1&&comPlay==2){
         // console.log("Computer Won");
         createELE("rock", "Paper", "Computer Won");
+        computerScore++;
     }else if (myPlay==1&&comPlay==3) {
         // console.log("Player Won");
         createELE("rock", "Scissor", "Player Won");
+        playerScore++;
     } else if(myPlay== 2&& comPlay==1){
         // console.log("Player Won");
         createELE("paper", "rock", "Player Won");
+        playerScore++;
     }else if(myPlay==2& comPlay==3){
         // console.log("Computer Won");
         createELE("paper", "Scissor", "Computer Won");
+        computerScore++;
     }else if(myPlay==3&& comPlay==1){
         // console.log("Computer Won");
         createELE("scissor", "rock", "Computer Won");
+        computerScore++;
     }else if(myPlay==3&&comPlay==2){
         // console.log("Player Won");
         createELE("scissor", "paper", "Player Won");
+        playerScore++;
     }else{
         console.log("TIE");
         createELE("Same as computer", "Same as player", "Tied");
@@ -63,4 +69,21 @@ function createELE(dataHuman, dataComputer, result){
     handHuman.innerHTML = dataHuman;
     computerHand.innerHTML = dataComputer;
     resultConst.innerHTML = result;
+}
+
+
+function scoreCheck(computerScore, playerScore){
+    if(computerScore>4){
+        promptDisplayer(1);
+    }else if(playerScore>4){
+        promptDisplayer(2)
+    }
+}
+
+function promptDisplayer(x){
+    if(x==1){
+
+    }else if(x==2){
+
+    }
 }
