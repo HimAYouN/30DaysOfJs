4827cd489a227a7ed3a21cea857efa9964d8876a
HimAYouN <himayoun7931@gmail.com>
2023-08-01 09:44:45 +0530

    Making of digital clock

---
+++ week3/digital_clock/main.js
@@ -0,0 +1,26 @@
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+
+// function dateDisplayer() {
+//   hour.innerHTML = date.getHours();
+//   minute.innerHTML = date.getMinutes();
+//   second.innerHTML = date.getSeconds();
+// }
+setInterval(function dateDisplayer() {
+  var date = new Date();
+    
+
+
+
+
+  hour.innerHTML = date.getHours();
+  minute.innerHTML = date.getMinutes();
+  
+  second.innerHTML = date.getSeconds();
+}, 1000);
+
+function dateFetcher() {
+  var date = new Date();
+  return date.getTime();
+}
