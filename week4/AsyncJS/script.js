574fbba5b570d16a787067d44b8af66e321f3c78
HimAYouN <himayoun7931@gmail.com>
2023-08-10 15:45:21 +0530

    Getting Somewhere

--- week4/AsyncJS/script.js
+++ week4/AsyncJS/script.js
@@ -1,4 +1,4 @@
-
+///////////Part 1/////////////
 
 // function y () {
 //     // console.log("Window");
@@ -11,3 +11,24 @@
 //     console.log("stopped")
 // }
 // const a = setTimeout(y, 3000);
+
+
+
+/////////////////Part 2/////////////
+const interval = null;
+var i = 0;
+function startTimerFnct(){
+    interval = setInterval(function timer(){
+        console.log("Hitesh", i++)
+    }, 1000);
+}
+
+function removeTimeFnct(){
+    clearInterval(interval);
+    console.log("Working");
+}
+
+
+// const timer= function(){
+    
+// }
