79bdf12355dc8ab9473d416625dda75c631fedcc
HimAYouN <himayoun7931@gmail.com>
2023-08-15 13:00:47 +0530

    Can't figure out....
    taking a break

--- week4/image_Slider/script.js
+++ week4/image_Slider/script.js
@@ -1,6 +1,8 @@
 const slides = document.querySelectorAll('.slide');
 // console.log(slides)
 var counter = 0;
- slides.forEach((slide, index) => {
-    slide.style.left = '${index * 100}%'
- });
+
+
+function plusSide(n){
+    slides[n].style.display = 'block';
+}
