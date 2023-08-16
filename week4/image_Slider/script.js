72c1add8e2282ed52447f81dbc5a629f735fb961
HimAYouN <himayoun7931@gmail.com>
2023-08-16 10:12:37 +0530

    Getting There!..

--- week4/image_Slider/script.js
+++ week4/image_Slider/script.js
@@ -1,8 +1,22 @@
 const slides = document.querySelectorAll('.slide');
 // console.log(slides)
-var counter = 0;
+var n = 0;
+let nextButton = document.getElementById('nextButton');
+nextButton.addEventListener('click', ()=>{
+    n = Math.floor(Math.random()*slides.length);
+    for(var i = 0; i<slides.length; i++){
+        if(i!=n){
+            slides[i].classList.add('slide');
+        }
+    }
+    console.log(n);
+    slides[n].classList.toggle('display');
+    slides[n].classList.toggle('slide');
 
+})
 
-function plusSide(n){
-    slides[n].style.display = 'block';
-}
+// function plusSide(n){
+//     slides[n].classList.toggle('.display');
+//     slides[n].classList.toggle('.slide');
+
+// }
