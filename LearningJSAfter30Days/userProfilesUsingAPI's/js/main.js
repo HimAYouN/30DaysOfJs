5d36411a89210e475954013d42ce787e94bb61e6
HimAYouN <himayoun7931@gmail.com>
2023-08-22 13:32:22 +0530

    Fixed that part
    Seperated all the objects into their own category, now time for styling 🥴

--- LearningJSAfter30Days/userProfilesUsingAPI's/js/main.js
+++ LearningJSAfter30Days/userProfilesUsingAPI's/js/main.js
@@ -1,35 +1,38 @@
-var username = [];
-var add = "";
-var email = "";
-var phone = "";
-const obj = [];
-
 const url = "https://jsonplaceholder.typicode.com/users";
-const users = async () => {
+
+const fetchData = async () => {
   const res = await fetch(url);
   const data = await res.json();
 
-  data.forEach((obj) => {
-    username.push(" <br>" + obj.name);
-    add = add + " <br>" + JSON.stringify(obj.address.city);
-    email = email + "<br>" + obj.email;
-    phone = phone + "<br>" + obj.phone;
+  const objects = data.map((obj) => {
+    return {
+      name: obj.name,
+      address: obj.address.city,
+      email: obj.email,
+      phone: obj.phone,
+    };
   });
-  //   document.getElementById('name').innerHTML = username   ;
-  //   document.getElementById('address').innerHTML = add;
-  //   document.getElementById('email').innerHTML = email;
-  //   document.getElementById('phone').innerHTML = phone;
 
-  // console.log(username)
-  displayObject(username(0), "usID1");
+  return objects;
 };
-users();
 
-function displayObject(element, divId) {
-  const div = document.getElementById(divId);
+const displayObjects = async () => {
+  const objects = await fetchData();
+
+  objects.forEach((object, index) => {
+    const divId = `usID${index + 1}`;
+    displayObject(object, divId);
+  });
+};
+
+function displayObject(object, divId) {
+  const div = document.querySelector(`.${divId}`);
   div.innerHTML = `
-      <h1>${element}</h2>
-      
-    `;
-  console.log(element);
+    <h1>Name: ${object.name}</h1>
+    <p>Address: ${object.address}</p>
+    <p>Email: ${object.email}</p>
+    <p>Phone: ${object.phone}</p>
+  `;
 }
+
+displayObjects();
