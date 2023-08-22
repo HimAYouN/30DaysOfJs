494114578d5bed639c5e1b919d6532cedf6d9cc5
HimAYouN <himayoun7931@gmail.com>
2023-08-22 12:43:25 +0530

    basic done

--- LearningJSAfter30Days/userProfilesUsingAPI's/js/main.js
+++ LearningJSAfter30Days/userProfilesUsingAPI's/js/main.js
@@ -1,17 +1,26 @@
+var username = "";
+  var add = "";
+  var email = "";
+  var phone = "";
+
+
 
 const url = "https://jsonplaceholder.typicode.com/users";
 const users =  async()=>{
   const res = await fetch(url);
   const data = await res.json();
-  var name = "";
-  var add = "";
+  
   data.forEach((obj)=>{
-    name = name + " <br>" + obj.name;
-    add = add+" <br>"+ JSON.stringify(obj.address);
+    username = username + " <br>" + obj.name;
+    add = add+" <br>"+ JSON.stringify(obj.address.city);
+    email = email +"<br>"+ obj.email;
+    phone = phone +"<br>"+ obj.phone;
     
   });
-  document.getElementById('name').innerHTML = name + add;
-//   document.getElementById('name').innerHTML = add;
+  document.getElementById('name').innerHTML = username ;
+  document.getElementById('address').innerHTML = add;
+  document.getElementById('email').innerHTML = email;
+  document.getElementById('phone').innerHTML = phone;
   
 }
 users();
