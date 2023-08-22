40f3a66026f928f3457e0a336bf6524c647d19fd
HimAYouN <himayoun7931@gmail.com>
2023-08-22 13:22:06 +0530

    trying...

--- LearningJSAfter30Days/userProfilesUsingAPI's/js/main.js
+++ LearningJSAfter30Days/userProfilesUsingAPI's/js/main.js
@@ -1,8 +1,8 @@
-var username = "";
+var username = [];
   var add = "";
   var email = "";
   var phone = "";
-
+    const obj = [];
 
 
 const url = "https://jsonplaceholder.typicode.com/users";
@@ -11,16 +11,29 @@ const users =  async()=>{
   const data = await res.json();
   
   data.forEach((obj)=>{
-    username = username + " <br>" + obj.name;
+    username.push(" <br>" + obj.name);
     add = add+" <br>"+ JSON.stringify(obj.address.city);
     email = email +"<br>"+ obj.email;
     phone = phone +"<br>"+ obj.phone;
-    
   });
-  document.getElementById('name').innerHTML = obj.name ;
-  document.getElementById('address').innerHTML = add;
-  document.getElementById('email').innerHTML = email;
-  document.getElementById('phone').innerHTML = phone;
+//   document.getElementById('name').innerHTML = username   ;
+//   document.getElementById('address').innerHTML = add;
+//   document.getElementById('email').innerHTML = email;
+//   document.getElementById('phone').innerHTML = phone;
   
+// console.log(username)
+displayObject(username(0), "usID1");
+
+
 }
 users();
+
+
+function displayObject(element, divId) {
+    const div = document.getElementById(divId);
+    div.innerHTML = `
+      <h1>${element}</h2>
+      
+    `;
+    console.log(element)
+  }
