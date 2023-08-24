2cb38a652dd619a8ad30b39fff0bfac2a4c46a9e
HimAYouN <himayoun7931@gmail.com>
2023-08-24 16:11:23 +0530

    Update App.js

--- week3/react-trail-app/src/App.js
+++ week3/react-trail-app/src/App.js
@@ -5,16 +5,43 @@ import ItemDate from "./components/ItemDate";
 
 
 function App() {
+  const itemTwoName = "SrufExel";
+  const response = [
+    {
+      itemName : "Nirma",
+      itemDate:{
+        itemDay: 20,
+        itemMonth : "August",
+        itemYear : 2000
+      },
+    },
+    {
+      itemName : "SrufExel",
+      itemDate:{
+        itemDay: 10,
+        itemMonth : "July",
+        itemYear : 2010
+      }
+    },
+    {
+      itemName : "555",
+      itemDate:{
+        itemDay: "02",
+        itemMonth : "May",
+        itemYear : 2012
+      }
+    }
+  ];
   return (
     <div>
-      <Item name="Nirma"></Item>
-      <ItemDate day="20" month="June" year="1990"></ItemDate>
+      <Item name={response[0].itemName}></Item>
+      <ItemDate day={response[0].itemDate.itemDay} month={response[0].itemDate.itemMonth} year={response[0].itemDate.itemYear}></ItemDate>
 
-      <Item name="SurfExel"></Item>
-      <ItemDate day="10" month="August" year="1999"></ItemDate>
+      <Item name={response[1].itemName}></Item>
+      <ItemDate day={response[1].itemDate.itemDay} month={response[1].itemDate.itemMonth} year={response[1].itemDate.itemYear}></ItemDate>
 
-      <Item name="555"></Item>
-      <ItemDate day="28" month="May" year="2007"></ItemDate>
+      <Item name={response[2].itemName}></Item>
+      <ItemDate day={response[2].itemDate.itemDay} month={response[2].itemDate.itemMonth} year={response[2].itemDate.itemYear}></ItemDate>
 
       <div className="App">Hello World</div>
     </div>
