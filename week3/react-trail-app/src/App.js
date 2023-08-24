84acd66d0620d8ab591dc6b2cde63bd825879203
HimAYouN <himayoun7931@gmail.com>
2023-08-24 15:41:49 +0530

    Basic React

--- week3/react-trail-app/src/App.js
+++ week3/react-trail-app/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 
 import Item from "./components/Item";
+import ItemDate from "./components/ItemDate";
 
 
 function App() {
   return (
     <div>
       <Item></Item>
+      <ItemDate></ItemDate>
       <div className="App">Hello World</div>
     </div>
   );
---
+++ week3/react-trail-app/src/components/ItemDate.js
@@ -0,0 +1,14 @@
+import './ItemDate.css';
+
+function ItemDate(){
+    return (
+        <div className='mfg-date'>
+            <span>20</span>
+            <span>June</span>
+            <span>2020</span>
+        </div>
+    );
+}
+
+
+export default ItemDate;
