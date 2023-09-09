055be035cb46b2d5627517165c0f3311e0c030ef
HimAYouN <himayoun7931@gmail.com>
2023-09-09 16:45:17 +0530

    Making Weather App

--- week3/WeatherApp/main.js
+++ week3/WeatherApp/main.js
@@ -1,3 +1,16 @@
-document.getElementById('locationButton').addEventListener('click', ()=>{
-    document.getElementById('locationEntered').value;
-})
+// const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=srinagar';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '3b385c1a4dmsh1b21ed9aaa71340p174110jsn9c45bb2b414e',
+// 		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+// 	}
+// };
+
+
+
+// fetch(url, options)
+// .then(response => response.json())
+// .then(response => console.log(response))
+// .catch(err => console.log(err));
+
