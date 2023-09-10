395dfba5deecd13032771a1c5cbdbf3bc8ead440
HimAYouN <himayoun7931@gmail.com>
2023-09-10 14:18:20 +0530

    Making Weather App

--- week3/WeatherApp/main.js
+++ week3/WeatherApp/main.js
@@ -7,19 +7,31 @@ const options = {
     "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
   },
 };
-
-fetch(url, options)
+const getWeather = (city)=>{
+  cityName.innerHTML = city
+  fetch(url, options)
   .then((response) => response.json())
   .then((response) => {
-    document.getElementById('cloud_pct.innerHTML') = response.cloud_pct;
-    temp = response.temp;
-    feels_like = response.feels_like;
-    humidity = response.humidity;
-    min_temp = response.min_temp;
-    max_temp = response.max_temp;
-    wind_speed = response.wind_speed;
-    wind_degrees = response.wind_degrees;
-    sunrise = response.sunrise;
-    sunset = response.sunset;
+    console.log(response)
+    cloud_pct.innerHTML = response.cloud_pct
+    temp.innerHTML = response.temp
+    feels_like.innerHTML = response.feels_like
+    humidity.innerHTML = response.humidity
+    min_temp.innerHTML = response.min_temp
+    max_temp.innerHTML = response.max_temp
+    wind_speed.innerHTML = response.wind_speed
+    wind_degrees.innerHTML = response.wind_degrees
+    sunrise.innerHTML = response.sunrise
+    sunset.innerHTML = response.sunset
   })
   .catch((err) => console.log(err));
+}
+
+
+submit.addEventListner('click', (e)=>{
+  e.preventDefault();
+  getWeather(city.value);
+});
+
+
+getWeather("sirnagar");
